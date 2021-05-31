66e7c5f44f5f993a44d17f4af5f816a6aff90bf4
Lesia <68897216+LesiaHoshovska@users.noreply.github.com>
2021-05-31 14:30:55 +0300

    Update Login.vue

--- Vue ДЗ-1/components/logIn/Login.vue
+++ Vue ДЗ-1/components/logIn/Login.vue
@@ -1,39 +1,45 @@
 <template>
-    <div>
-        <label>
-            Login:
-            <input type="text" v-model="log">
-        </label> <br>
-        <label>
-            Password:
-            <input type="text" v-model="pass">
-        </label><br>
-        <button @click="validateLog">Go</button>
-    </div>
+  <div>
+    <label>
+      Login:
+      <input type="text" v-model="log" />
+    </label>
+    <br />
+    <label>
+      Password:
+      <input type="text" v-model="pass" /> </label
+    ><br />
+    <button @click="validateLog">Go</button>
+  </div>
 </template>
-
 
 <script>
 export default {
-    name: "Login",
-    data() {
-        return {
-           log : '',
-           pass: ""
+  name: "Login",
+  data() {
+    return {
+      log: "",
+      pass: "",
+      logArr: [
+        { log: "password", pass: "p@ssw0rd" },
+        { log: "11545", pass: "564" },
+        { log: "nianai", pass: "68547" },
+      ],
+    };
+  },
+  methods: {
+    validateLog() {
+      for (let i = 0; i < this.logArr.length; i++) {
+        if (
+          this.log.value === this.logArr[i].log &&
+          this.pass.value === this.logArr[i].pass
+        ) {
+          return alert("Welcome");
+        } else {
+          return alert("invalid pass or log");
         }
+      }
     },
-    methods: {
-        validateLog() {
-            let logArr = [{log: "password", pass: "p@ssw0rd"}, {log: "11545", pass: "564"}, {log: "nianai", pass: "68547"}]
-            for (let i = 0; i < logArr.length; i++) {
-                if (this.log===logArr[i].log && this.pass===logArr[i].pass) {
-                    return "Welcome"     
-                }else{
-                alert("invalid pass or log")
-            }
-                
-            }
-        }
-    },
-}
+  },
+};
 </script>
