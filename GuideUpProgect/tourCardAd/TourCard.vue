6145b8549862efec9242a6b44a2b131743a60570
Lesia <68897216+LesiaHoshovska@users.noreply.github.com>
2021-06-22 22:29:25 +0300

    Add files via upload

---
+++ GuideUpProgect/tourCardAd/TourCard.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <div><img :src="imgSrc" alt="Everest Base camp"></div>
+        <div>
+            <h1>{{tourTittle}}</h1>
+        </div>
+        <div>
+            <h1>{{toursDaysAmount}}</h1>
+        </div>
+        <div>
+            <h1>{{toursTotalDistance}}</h1>
+        </div>
+        <div>
+            <h1>{{toursPrice}}</h1>
+        </div>
+
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'TourCard',
+        props: {
+            imgSrc: {
+                type: String,
+                default: 'https://extremeguide.pro/wp-content/uploads/2018/12/BXiM0Jy2AY8.jpg' 
+            },
+            tourTittle: {
+                type: String,
+                default: 'Trekking to Everest base Camp'
+            },
+            toursDaysAmount:{
+                type: Number,
+                default: 14
+            },
+            toursTotalDistance:{
+                type: Number,
+                default: 90
+            },
+            toursPrice: {
+                type: String,
+                default: '950$'
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
