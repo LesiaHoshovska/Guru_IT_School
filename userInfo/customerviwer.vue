5a12a790ab4cc769dad58c6e40f4c30c49b3e3ef
Lesia <68897216+LesiaHoshovska@users.noreply.github.com>
2021-06-25 15:47:59 +0300

    Update customerviwer.vue

--- userInfo/customerviwer.vue
+++ userInfo/customerviwer.vue
@@ -1,30 +1,39 @@
 <template>
-    <div>
-        <customer-card v-model="name" 
+<div>
+    <customer-card  v-model="name" 
         :customer-phone-number.sync="phoneNumber"
         :customer-age.sync="age"/>
+        <div>
+            Customer name: {{name}}
+        </div>
+        <div>
+            Customer phone number: {{phoneNumber}}
+        </div>
+        <div>
+            Customer age {{age}}
+        </div>
 
-        
-
-
-    </div>
+</div>
+  
+       
 </template>
 
 <script>
-import { CustomerCard } from "./customercard/customercard.vue";
-    export default {
-        name: 'CustomerViwer',
-        components: {CustomerCard},
-        data() {
+import customerCard from './customerCard/customerCard.vue'
+export default {
+    name: 'ShopCustomersApp',
+  components: { customerCard },
+  data() {
             return {
                 name: "John",
                 age: 25,
                 phoneNumber: null
             }
         },
-    }
+
+}
 </script>
 
-<style lang="scss" scoped>
+<style>
 
 </style>
