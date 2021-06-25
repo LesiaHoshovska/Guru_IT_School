12b5864c41c168d1a573d353714303b67e85737b
Lesia <68897216+LesiaHoshovska@users.noreply.github.com>
2021-06-25 16:02:48 +0300

    Add files via upload

---
+++ shopCustomersApp/customersList/customersList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+      <div>
+            Customer name: {{name}}
+        </div>
+        <div>
+            Customer phone number: {{phoneNumber}}
+        </div>
+        <div>
+            Customer age {{age}}
+        </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+name: 'CustomersList',
+props: {
+    name: {
+        type: String,
+        default: 'No name'
+    },
+    phoneNumber: {
+        type: Number,
+        default: null
+    },
+    age: {
+        type: Number,
+        default: 25
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
