1ab4fd33fef45794f7318b9f7094fa4bf695aa17
Lesia <68897216+LesiaHoshovska@users.noreply.github.com>
2021-06-25 15:51:35 +0300

    Add files via upload

---
+++ shopCustomersApp/customerCard/customerCard.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div>
+      <label>
+      Name 
+      <input type="text" :value="value"
+      @input="$emit('input', $event.target.value)">
+    </label>
+    <label>
+      Phone Number 
+      <input type="number" :min="0"
+      :value="customerPhoneNumber"
+      @input="$emit('update:customerPhoneNumber', $event.target.value)"> 
+    </label>
+    <label>
+      Age 
+      <input type="number"
+      :value="customerAge"
+      @input="$emit('update:customerAge', $event.target.value)"
+      >
+    </label>
+    </div>
+   </div>
+</template>
+
+<script>
+export default {
+name: 'CustomerCard',
+props: {
+      value: {
+        type: String,
+        default: 'No name'
+      },
+      customerPhoneNumber:{
+        type: Number,
+        default: null
+      },
+      customerAge: {
+        type: Number,
+        default: 25
+      }
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ shopCustomersApp/shopCustomersApp.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+    <customer-card  v-model="name" 
+        :customer-phone-number.sync="phoneNumber"
+        :customer-age.sync="age"/>
+        <div>
+            Customer name: {{name}}
+        </div>
+        <div>
+            Customer phone number: {{phoneNumber}}
+        </div>
+        <div>
+            Customer age {{age}}
+        </div>
+
+</div>
+  
+       
+</template>
+
+<script>
+import customerCard from './customerCard/customerCard.vue'
+export default {
+    name: 'ShopCustomersApp',
+  components: { customerCard },
+  data() {
+            return {
+                name: "John",
+                age: 25,
+                phoneNumber: null
+            }
+        },
+
+}
+</script>
+
+<style>
+
+</style>
